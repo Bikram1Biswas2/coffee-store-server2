6aeb36f68b4dd2c6fdaed5f335a3743b1dbc9c9a
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-11-26 02:38:32 +0530

    create delete and update method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -32,11 +32,20 @@ async function run() {
     const coffeeCollection = client.db('coffeeStoreDB').collection('coffee')
 
 
-    // read
+    // get the data
     app.get('/coffee',async(req,res)=>{
         const cursor = coffeeCollection.find()
         const result = await cursor.toArray()
         res.send(result)
+    })
+
+    // update 
+    app.get('/coffee/:id', async(req,res)=>{
+        const id = req.params.id
+        const query = {_id:new ObjectId(id)}
+        const result = await coffeeCollection.findOne(query)
+        res.send(result)
+
     })
 
 
@@ -48,6 +57,14 @@ async function run() {
         res.send(result)
     })
 
+
+    // delete
+    app.delete('/coffee/:id',async(req,res)=>{
+        const id = req.params.id
+        const query = {_id:new ObjectId(id)}
+        const result = await coffeeCollection.deleteOne(query)
+        res.send(result)
+    })
 
 
 
