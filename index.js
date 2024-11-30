9fe385a4c1e2b1be90a7dbbe7501a208688bbdd4
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-11-30 11:45:44 +0530

    delete the user from the database

--- index.js
+++ index.js
@@ -105,6 +105,14 @@ async function run() {
       res.send(result)
     })
 
+    // 3-delete
+    app.delete('/users/:id',async(req,res)=>{
+      const id = req.params.id 
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
